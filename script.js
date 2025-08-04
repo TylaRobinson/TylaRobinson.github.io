8044434bc17984fe26ad0ae1331c4d54863fc441
TylaRobinson <tyla.robinson@southlandgirls.school.nz>
2025-08-05 09:53:09 +1200

    Savings Form Trial 1
    Added and completed some debugging of my savings form trial #1. Need to push to github as accessing via file is not allowing me to link to manifest.json and

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-document.getElementById("saveButton").addEventListener("click", saveGoal);
-function saveGoal(){
-	const myGoal = document.getElementById("goalInput").value
-	localStorage.setItem("goal", myGoal);
-	document.getElementById("goal-spot").innerHTML = myGoal
-}
+document.getElementById("savingsForm").addEventListener("submit", function (e) {
+	e.preventDefault();
+	 
+	const goalAmount = parseFloat(document.getElementById("goalAmount").value);
+	localStorage.setItem("goalAmount", goalAmount);
+	const goalName = document.getElementById("goalName").value;
+	localStorage.setItem("goalName", goalName);
+	const targetDate = new Date (document.getElementById("targetDate").value);
+	localStorage.setItem("targetDate", targetDate);
+	const frequency = document.getElementById("frequency").value
+	localStorage.setItem("frequency", frequency);
+
+	const today = new Date();
+	const timeDiff = targetDate - today;
+	
+	if (timeDiff <= 0 || isNan(goalAmount)) {
+		document.getElementById("result").innerHTML = "<strong>Please enter a valid future date and amount. </strong>";
+		return;
+	}
+	
+	const weeks = Math.ceil(timeDiff / (1000 * 60 * 60 * 24 * 7));
+	let periods;
+	
+	if (frequency === "weekly") periods = weeks;
+	else if (frequency === "fortnightly") periods = Math.ceil(weeks/2);
+	else if (frequency === "monthly") periods = Math.ceil((targetDate.getFullYear() - today.getFullYear()) *12 + targetDate.getMonth() - today.getMonth());
+	
+	const perPeriod = goalAmount / periods; 
+	
+	document.getElementById("result").innerHTML = 
+		`<strong> To save $${goalAmount.toFixed(2)} for "${goalName || "your goal"}" by ${targetDate.toDateString()}, <br>
+		You need to save $${perPeriod.toFixed(2)} every ${frequency}.</strong>`;
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
+//document.getElementById("saveButton").addEventListener("click", saveGoal);
+//function saveGoal(){
+//	const myGoal = document.getElementById("goalInput").value
+//	localStorage.setItem("goal", myGoal);
+//	document.getElementById("goal-spot").innerHTML = myGoal
+//}
